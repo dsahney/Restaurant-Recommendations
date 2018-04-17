84e3d91aed76e7e55ad93e5645fe20124e2d8fb2
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-04-16 23:43:23 -0400

    Create restaurants.py

---
+++ restaurants.py
@@ -0,0 +1,158 @@
+"""
+A restaurant recommendation system.
+
+Here are some example dictionaries.  These correspond to the information in
+restaurants_small.txt.
+
+Restaurant name to rating:
+# dict of {str: int}
+{'Georgie Porgie': 87,
+ 'Queen St. Cafe': 82,
+ 'Dumplings R Us': 71,
+ 'Mexican Grill': 85,
+ 'Deep Fried Everything': 52}
+
+Price to list of restaurant names:
+# dict of {str, list of str}
+{'$': ['Queen St. Cafe', 'Dumplings R Us', 'Deep Fried Everything'],
+ '$$': ['Mexican Grill'],
+ '$$$': ['Georgie Porgie'],
+ '$$$$': []}
+
+Cuisine to list of restaurant names:
+# dict of {str, list of str}
+{'Canadian': ['Georgie Porgie'],
+ 'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
+ 'Malaysian': ['Queen St. Cafe'],
+ 'Thai': ['Queen St. Cafe'],
+ 'Chinese': ['Dumplings R Us'],
+ 'Mexican': ['Mexican Grill']}
+
+With this data, for a price of '$' and cuisines of ['Chinese', 'Thai'], we
+would produce this list:
+
+    [[82, 'Queen St. Cafe'], [71, 'Dumplings R Us']]
+"""
+
+# The file containing the restaurant data.
+FILENAME = 'restaurants_small.txt'
+
+
+def recommend(file, price, cuisines_list):
+    """(file open for reading, str, list of str) -> list of [int, str] list
+
+    Find restaurants in file that are priced according to price and that are
+    tagged with any of the items in cuisines_list.  Return a list of lists of
+    the form [rating%, restaurant name], sorted by rating%.
+    """
+
+    # Read the file and build the data structures.
+    # - a dict of {restaurant name: rating%}
+    # - a dict of {price: list of restaurant names}
+    # - a dict of {cusine: list of restaurant names}
+    name_to_rating, price_to_names, cuisine_to_names = read_restaurants(file)
+
+
+    # Look for price or cuisines first?
+    # Price: look up the list of restaurant names for the requested price.
+    names_matching_price = price_to_names[price]
+
+    # Now we have a list of restaurants in the right price range.
+    # Need a new list of restaurants that serve one of the cuisines.
+    names_final = filter_by_cuisine(names_matching_price, cuisine_to_names, cuisines_list)
+
+    # Now we have a list of restaurants that are in the right price range and serve the requested cuisine.
+    # Need to look at ratings and sort this list.
+    result = build_rating_list(name_to_rating, names_final)
+
+    # We're done!  Return that sorted list.
+    return result
+
+def build_rating_list(name_to_rating, names_final):
+    """ (dict of {str: int}, list of str) -> list of list of [int, str]
+
+    Return a list of [rating%, restaurant name], sorted by rating%
+
+    >>> name_to_rating = {'Georgie Porgie': 87,
+     'Queen St. Cafe': 82,
+     'Dumplings R Us': 71,
+     'Mexican Grill': 85,
+     'Deep Fried Everything': 52}
+    >>> names = ['Queen St. Cafe', 'Dumplings R Us']
+    [[82, 'Queen St. Cafe'], [71, 'Dumplings R Us']]
+    """
+    new_list = []
+    for y in range(len(names_final)):
+        new_list.append([name_to_rating[names_final[y]], names_final[y]])
+    return new_list.reverse()
+
+def filter_by_cuisine(names_matching_price, cuisine_to_names, cuisines_list):
+    """ (list of str, dict of {str: list of str}, list of str) -> list of str
+
+    >>> names = ['Queen St. Cafe', 'Dumplings R Us', 'Deep Fried Everything']
+    >>> cuis = 'Canadian': ['Georgie Porgie'],
+     'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
+     'Malaysian': ['Queen St. Cafe'],
+     'Thai': ['Queen St. Cafe'],
+     'Chinese': ['Dumplings R Us'],
+     'Mexican': ['Mexican Grill']}
+    >>> cuisines = ['Chinese', 'Thai']
+    >>> filter_by_cuisine(names, cuis, cuisines)
+    ['Queen St. Cafe', 'Dumplings R Us']
+    """
+    list_accumulator = []
+    for i in range(len(cuisines_list)):
+        if cuisines_list[i] in cuisine_to_names:
+            list_accumulator += cuisine_to_names[cuisines_list[i]]
+            
+    names_final = []
+    for x in range(len(list_accumulator)):
+        if list_accumulator[x] in names_matching_price:
+            names_final.append(list_accumulator[x])
+
+    return names_final
+    
+def read_restaurants(file):
+    """ (file) -> (dict, dict, dict)
+
+    Return a tuple of three dictionaries based on the information in the file:
+
+    - a dict of {restaurant name: rating%}
+    - a dict of {price: list of restaurant names}
+    - a dict of {cusine: list of restaurant names}
+    """
+# read the file line by line and accumulate info about the restaurants.
+    name_to_rating = {}
+    price_to_names = {'$': [], '$$': [], '$$$': [], '$$$$': []}
+    cuisine_to_names = {}
+
+    file = open(FILENAME, 'r')
+    a = 1 # use an increasing number, a.
+    for line in file:
+        if line.strip():
+            line = line.strip('\n')
+
+            if a == 1: # get the restaurant's name.
+                name_restaurant = line
+
+            if a == 2:
+                rating_restaurant = line
+                name_to_rating[name_restaurant] = rating_restaurant
+            elif a == 3:
+                price_restaurant = line
+                price_to_names[price_restaurant].append(name_restaurant)
+            elif a == 4:
+                cuisines_list = line.split(',')
+                for cuisine in cuisines_list:
+                    cuisine_to_names = name_restaurant
+        else:
+            a = 0
+        a = a + 1
+
+    print(name_to_rating)
+    print(price_to_names)
+    print(cuisine_to_names)
+
+    return (name_to_rating, price_to_names, cuisine_to_names)
+
+    file.close()
